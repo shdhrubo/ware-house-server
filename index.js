1fe38e17e682a0f225d35654db4a8fe5b4c613a7
shdhrubo <shorifulhabib.iit@gmail.com>
2022-05-03 00:00:55 +0600

    db connected

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//checking if everything is ok
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors()); //middleware
+app.use(express.json()); //middleware for undefined
+app.get("/", (req, res) => {
+  res.send("connected");
+});
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
