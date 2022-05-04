2ef30be72151fe7a939f0df97e011d6c67ca1b74
shdhrubo <shorifulhabib.iit@gmail.com>
2022-05-04 16:33:40 +0600

    mongo db connected and inventories api created

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const express = require("express");
+
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
@@ -6,6 +9,31 @@ const port = process.env.PORT || 5000;
 
 app.use(cors()); //middleware
 app.use(express.json()); //middleware for undefined
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.baf5z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const inventoriesCollection = client
+      .db("inventoryManagement")
+      .collection("inventories");
+    app.get("/inventories", async (req, res) => {
+      const query = {};
+      const cursor = inventoriesCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("connected");
 });
